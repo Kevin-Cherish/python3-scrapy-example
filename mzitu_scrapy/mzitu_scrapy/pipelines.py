d4ac7a86cb41b7819db05c3fac0e0e67504a1f40
wkd-lidashuang <lidashuang_wkd@163.com>
2018-11-17 13:00:10 +0800

    update filepath

--- mzitu_scrapy/mzitu_scrapy/pipelines.py
+++ mzitu_scrapy/mzitu_scrapy/pipelines.py
@@ -10,6 +10,19 @@ from scrapy.exceptions import DropItem
 
 
 class MzituScrapyPipeline(ImagesPipeline):
+    def file_path(self, request, response=None, info=None):
+        """
+        :param request: 每一个图片下载管道请求
+        :param response:
+        :param info:
+        :param strip :清洗Windows系统的文件夹非法字符，避免无法创建目录
+        :return: 每套图的分类目录
+        """
+        item = request.meta['item']
+        folder_strip = item['name']
+        image_guid = request.url.split('/')[-1]
+        filename = u'full/{0}/{1}'.format(folder_strip, image_guid)
+        return filename
 
     def get_media_requests(self, item, info):
         """
